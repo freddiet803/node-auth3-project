ab7ea28ff20de6a4f1061a5ae2380deb16752da5
Freddie <fthompson803@live.com>
2020-02-19 12:27:36 -0500

    initial commit adding dependecies

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const server = express();
+const cors = require('cors');
+
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+  res.send('api up and running');
+});
+
+module.exports = server;
